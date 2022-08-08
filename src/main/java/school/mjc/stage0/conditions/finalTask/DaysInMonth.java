b46e46cd464d507af114c0a2023028c80233f81f
Maiko <15mmmode@gmail.com>
2022-08-08 20:44:56 +0400

    finish

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0 || month < 0 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        switch (month) {
+            case (1), (3), (5), (7), (8), (10), (12) -> System.out.println(31);
+            case (2) -> {
+                if (year % 4 != 0 || year == 1900) {
+                    System.out.println(28);
+                } else if (year % 4 == 0) {
+                    System.out.println(29);
+                }
+            }
+            default -> System.out.println(30);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -1,7 +1,11 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import javax.swing.*;
+
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        String result = firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide
+                && secondSide + thirdSide > thirdSide ? "this is a valid triangle" : "it's not a triangle";
+        System.out.println(result);
     }
 }
